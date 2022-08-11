463bc0a60e9334808fcf382362311f9300c5d59a
azharanowar <azharanowar@gmail.com>
2022-08-11 12:43:37 +0600

    Assignment program three completed

--- assignmentFour.js.js
+++ assignmentFour.js.js
@@ -23,16 +23,43 @@ function radianToDegree( radian ) {
     3. The function will return true if that is a Javascript file. Otherwise, return false.
  */
 
-    function isJavaScriptFile( fileName ) {
-        if ( typeof fileName !== 'string' ) {
-            return "Please enter a valid file name!";
-        }
-
-        if ( fileName.endsWith( '.js' ) ) {
-            return true;
-        } else {
-            return false;
-        }
+function isJavaScriptFile( fileName ) {
+    if ( typeof fileName !== 'string' ) {
+        return "Please enter a valid file name!";
     }
-    
-    // console.log( isJavaScriptFile( "image.jpg.js" ) );
+
+    if ( fileName.endsWith( '.js' ) ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// console.log( isJavaScriptFile( "image.jpg.js" ) );
+
+
+/*
+    1. The function name will be exact oilPrice
+    2. The function will have 3 perameters, 
+*/
+
+function oilPrice( dieselQuantity, petrolQuantity, octaneQuantity ) {
+
+    if ( typeof dieselQuantity !== 'number' || typeof petrolQuantity !== 'number' || typeof octaneQuantity !== 'number' ) {
+        return "Please enter valid quantity of diesel, petrol, and octane! The quantity must be in number.";
+    }
+
+    const dieselPrice = 114;
+    const petrolPrice = 130;
+    const octanePrice = 135;
+
+    const dieselTotalPrice = dieselPrice * dieselQuantity;
+    const petrolTotalPrice = petrolPrice * petrolQuantity;
+    const octaneTotalPrice = octanePrice * octaneQuantity;
+
+    const totalPrice = dieselTotalPrice + petrolTotalPrice + octaneTotalPrice;
+
+    return totalPrice;
+}
+
+// console.log( oilPrice( 1, 0, 2 ) );
